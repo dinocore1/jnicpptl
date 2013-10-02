f35a4b948580cec490f86992d23ccbafcbd40099
Paul Soucy <paul@paul-VirtualBox.(none)>
2013-10-01 22:01:33 -0400

    whatever

--- include/jnicpptl/jnimethod.h
+++ include/jnicpptl/jnimethod.h
@@ -6,69 +6,89 @@
 #include <jni.h>
 #include <string>
 
-class JniProxy;
 
-template<class R, typename... P>
-R JniMethodInvoke(JNIEnv* env, jclass clazz, jmethodID method, P... q);
-
-template<class R, typename... P>
-R JniMethodInvoke(JNIEnv* env, jobject inst, jmethodID method, P... q);
+#include <jnicpptl/jniproxy.h>
+#include <jnicpptl/jniobject.h>
+#include <jnicpptl/jniexception.h>
 
 
+template<bool isStatic>
 class JniMethod_base {
 
 protected:
 	const std::string mMethodName;
 	const std::string mMethodSignature;
 	jmethodID mMethodID;
+	JniProxy* mProxy;
 
 public:
-	JniMethod_base(const char* name, const char* sig)
+	JniMethod_base(JniProxy* proxy, const char* name, const char* sig)
 	: mMethodName(name)
 	, mMethodSignature(sig)
  	, mMethodID(NULL)
+	, mProxy(proxy)
 	{}
+
+	jmethodID getMethodID();
 };
+
+template<>
+jmethodID JniMethod_base<true>::getMethodID() {
+	if(mMethodID == NULL) {
+		mMethodID = mProxy->getJNIEnv()->GetStaticMethodID(mProxy->getClass(), mMethodName.c_str(), mMethodSignature.c_str());
+	}
+	return mMethodID;
+}
+
+template<>
+jmethodID JniMethod_base<false>::getMethodID() {
+	if(mMethodID == NULL) {
+		mMethodID = mProxy->getJNIEnv()->GetMethodID(mProxy->getClass(), mMethodName.c_str(), mMethodSignature.c_str());
+	}
+	return mMethodID;
+}
 
 template<class T, bool isStatic>
 class JniMethod;
 
-template<class R, typename... P>
-class JniMethod<R(P...), true> : public JniMethod_base {
 
-protected:
-	JniProxy* mProxy;
-
-public:
-	JniMethod(JniProxy* proxy, const char* name, const char* sig)
-	: JniMethod_base(name, sig)
-	, mProxy(proxy)
-	{}
-
-	jmethodID getMethodID();
-
-	R operator() (P... q);
-};
-
-template<class R, typename... P>
-class JniMethod<R(P...), false> : public JniMethod_base {
-protected:
-	JniProxy* mProxy;
-
-public:
-	JniMethod(JniProxy* proxy, const char* name, const char* sig)
-	: JniMethod_base(name, sig)
-	, mProxy(proxy)
-	{}
-
-	jmethodID getMethodID();
-
-	R operator() (P... q);
-
+#define JniMethod_ACCESSORS(TYPE, NAME_FRAG) \
+template<typename... P> \
+class JniMethod<TYPE(P...), false> : public JniMethod_base<false> { \
+public:	\
+	JniMethod(JniProxy* proxy, const char* name, const char* sig) \
+	: JniMethod_base<false>(proxy, name, sig) \
+	{} \
+	TYPE operator() (P... q) { \
+		return (TYPE)mProxy->getJNIEnv()->Call ## NAME_FRAG ## Method(mProxy->getInstance(), getMethodID(), q...); \
+	} \
+}; \
+template<typename... P> \
+class JniMethod<TYPE(P...), true> : public JniMethod_base<true> { \
+public:	\
+	JniMethod(JniProxy* proxy, const char* name, const char* sig) \
+	: JniMethod_base<true>(proxy, name, sig) \
+	{} \
+	TYPE operator() (P... q) { \
+		return (TYPE)mProxy->getJNIEnv()->CallStatic ## NAME_FRAG ## Method(mProxy->getClass(), getMethodID(), q...); \
+	} \
 };
 
 
+JniMethod_ACCESSORS(void, Void)
+JniMethod_ACCESSORS(jstring, Object)
+JniMethod_ACCESSORS(jobject, Object)
+JniMethod_ACCESSORS(jclass, Object)
+JniMethod_ACCESSORS(jboolean, Boolean)
+JniMethod_ACCESSORS(jbyte, Byte)
+JniMethod_ACCESSORS(jchar, Char)
+JniMethod_ACCESSORS(jshort, Short)
+JniMethod_ACCESSORS(jint, Int)
+JniMethod_ACCESSORS(jlong, Long)
+JniMethod_ACCESSORS(jfloat, Float)
+JniMethod_ACCESSORS(jdouble, Double)
 
+#undef JniMethod_ACCESSORS
 
 
 
--- src/jnimethod.cpp
+++ src/jnimethod.cpp
@@ -1,38 +1,11 @@
 
-#include <jnicpptl/jnimethod.h>
-#include <jnicpptl/jniproxy.h>
-#include <jnicpptl/jniobject.h>
-#include <jnicpptl/jniexception.h>
 
 
-#define JniMethod_ACCESSORS(TYPE, NAME_FRAG) \
-template<class R, typename... P> \
-TYPE JniMethodInvoke(JNIEnv* env, jclass clazz, jmethodID method, P... q) { \
-	return (TYPE)env->CallStatic ## NAME_FRAG ##Method(clazz, method, q...); \
-} \
-template<class R, typename... P> \
-TYPE JniMethodInvoke(JNIEnv* env, jobject inst, jmethodID method, P... q) { \
-	return (TYPE)env->Call ## NAME_FRAG ## Method(inst, method, q...); \
-}
 
-
-JniMethod_ACCESSORS(void, Void)
-JniMethod_ACCESSORS(jstring, Object)
-JniMethod_ACCESSORS(jobject, Object)
-JniMethod_ACCESSORS(jclass, Object)
-JniMethod_ACCESSORS(jboolean, Boolean)
-JniMethod_ACCESSORS(jbyte, Byte)
-JniMethod_ACCESSORS(jchar, Char)
-JniMethod_ACCESSORS(jshort, Short)
-JniMethod_ACCESSORS(jint, Int)
-JniMethod_ACCESSORS(jlong, Long)
-JniMethod_ACCESSORS(jfloat, Float)
-JniMethod_ACCESSORS(jdouble, Double)
-
-#undef JniMethod_ACCESSORS
 
 /****** Static JniMethod *******/
 
+/*
 template<class R, typename... P>
 jmethodID JniMethod<R(P...), true>::getMethodID()
 {
@@ -48,6 +21,7 @@ jmethodID JniMethod<R(P...), true>::getMethodID()
 	return retval;
 }
 
+
 template<class R, typename... P>
 R JniMethod<R(P...), true>::operator() (P... q)
 {
@@ -55,11 +29,13 @@ R JniMethod<R(P...), true>::operator() (P... q)
 		return JniMethodInvoke(mProxy->getJNIEnv(), mProxy->getClass(), getMethodID(), q...);
 	}
 }
-
+*/
 
 /******** Instance JniMethod ********/
+
+/*
 template<class R, typename... P>
-jmethodID JniMethod<R(P...), false>::getMethodID()
+jmethodID JniMethod<false, R, P...>::getMethodID()
 {
 	jmethodID retval = mMethodID;
 	if(mMethodID == NULL && mProxy != NULL){
@@ -73,11 +49,5 @@ jmethodID JniMethod<R(P...), false>::getMethodID()
 	return retval;
 }
 
-template<class R, typename... P>
-R JniMethod<R(P...), false>::operator() (P... q)
-{
-	if(mProxy != NULL) {
-		return JniMethodInvoke(mProxy->getJNIEnv(), mProxy->getInstance(), getMethodID(), q...);
-	}
-}
 
+*/
